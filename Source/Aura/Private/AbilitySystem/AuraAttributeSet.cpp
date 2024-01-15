00019c4884529171ad2be04c5104760ce96a7aa8
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-01-15 19:03:13 +0800

    26. Attribute Accessors
    -using define to add some accessor function about attributes.
    -init attributes on constructor.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -8,7 +8,10 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
