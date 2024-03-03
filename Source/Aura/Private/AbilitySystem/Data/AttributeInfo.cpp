805bb712d9a1db0c7e25a6c8e514aee6af7157fd
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-03 20:26:12 +0800

    89. Attribute Info Data Asset
    -add a new attribute info data asset to store attribute infomation.

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,23 @@
+// Copyright syxxmyl
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound /*= false*/) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
