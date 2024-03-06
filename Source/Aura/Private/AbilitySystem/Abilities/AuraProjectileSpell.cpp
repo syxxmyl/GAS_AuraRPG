0f9d2f9e9fb5b5e46d22e62158469badee68ec99
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-06 19:06:33 +0800

    110. Aura Projectile Spell
    -create aura projectile spell class.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,13 @@
+// Copyright syxxmyl
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++)"), true, true, FLinearColor::Yellow, 3);
+}
