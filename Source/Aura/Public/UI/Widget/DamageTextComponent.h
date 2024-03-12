6dc98b4e8714f9f7cc7b261da549c1e67bc37a38
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-12 15:52:12 +0800

    140. Floating Text Widget
    -create a floating text widget.
    -create a widget component to set text value.

---
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -0,0 +1,20 @@
+// Copyright syxxmyl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetDamageText(float Damage);
+};
