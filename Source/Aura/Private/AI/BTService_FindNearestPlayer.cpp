da166342ee91d9644f62524769aab7e3716e2ff0
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-17 17:05:07 +0800

    167. Finding the Nearest Player
    -finding nearest player and move to the player.

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,6 +4,7 @@
 #include "AI/BTService_FindNearestPlayer.h"
 #include "AIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
 
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
@@ -18,6 +19,25 @@ void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, u
 		const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
 		TArray<AActor*> ActorsWithTag;
 		UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+
+
+		float ClosestDistance = TNumericLimits<float>::Max();
+		AActor* ClosestActor = nullptr;
+		for (AActor* Actor : ActorsWithTag)
+		{
+			if (IsValid(Actor) && IsValid(OwningPawn))
+			{
+				const float Distance = OwningPawn->GetDistanceTo(Actor);
+				if (Distance < ClosestDistance)
+				{
+					ClosestDistance = Distance;
+					ClosestActor = Actor;
+				}
+			}
+		}
+
+		UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetToFollowSelector, ClosestActor);
+		UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTargetSelector, ClosestDistance);
 	}
 
 
