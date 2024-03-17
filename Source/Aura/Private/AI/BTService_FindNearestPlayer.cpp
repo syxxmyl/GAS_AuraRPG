7781ea5fa5079c3762e1b6fcd8b2fea9517526f5
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-17 16:17:44 +0800

    166. Blackboard Keys
    -add two blackboard keys, actor to follow and distance between owner actor to follow actor.

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -3,12 +3,22 @@
 
 #include "AI/BTService_FindNearestPlayer.h"
 #include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, *AIOwner->GetName());
-	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Green, *ActorOwner->GetName());
+	// GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, *AIOwner->GetName());
+	// GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Green, *ActorOwner->GetName());
+
+	if (APawn* OwningPawn = AIOwner->GetPawn())
+	{
+		const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+		TArray<AActor*> ActorsWithTag;
+		UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+	}
+
+
 }
