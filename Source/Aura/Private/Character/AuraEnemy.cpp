f64a92e4b713cc615a87356f16011227baa21543
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-18 15:48:36 +0800

    169. Behavior Tree Decorators
    -fix up ranged enemy doesn't do anything.

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -50,7 +50,7 @@ void AAuraEnemy::PossessedBy(AController* NewController)
 		AuraAIController->GetBlackboardComponent()->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
 		AuraAIController->RunBehaviorTree(BehaviorTree);
 		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"), false);
-		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"), CharacterClass != ECharacterClass::Warrior);
+		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("RangedAttacker"), CharacterClass != ECharacterClass::Warrior);
 	}
 }
 
