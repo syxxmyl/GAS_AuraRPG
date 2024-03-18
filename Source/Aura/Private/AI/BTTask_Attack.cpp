3a8435abf22a213847c78ba16b4605714c7d4c46
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-18 15:50:02 +0800

    170. Attack Behavior Tree Task
    -create attack task for behavior tree.

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,10 @@
+// Copyright syxxmyl
+
+
+#include "AI/BTTask_Attack.h"
+
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
