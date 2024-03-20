29f72ad0b8154c1982b9647812b5f5b2a264253e
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-20 12:18:49 +0800

    198. Slingshot Attack Montage
    -using montage notify to change attack animation and pouch location.

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -43,7 +43,7 @@ protected:
 	
 	virtual void InitAbilityActorInfo();
 
-	UPROPERTY(EditAnywhere, Category = "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
