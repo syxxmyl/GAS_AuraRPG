9a92f69dd4c7207a5885437eef2cc6158817b981
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-21 19:57:36 +0800

    225. Elementalist Behavior Tree
    -create a new behavior tree for elementalist enemy.

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -35,12 +35,12 @@ protected:
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> Sphere;
+
 private:
 	UPROPERTY(EditDefaultsOnly)
 	float LifeSpan = 15.0f;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
