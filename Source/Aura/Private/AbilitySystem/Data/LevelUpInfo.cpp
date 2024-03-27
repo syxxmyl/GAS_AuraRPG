aef33fe44cbef04a856dd3e321c7a6c02f8d0919
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-03-27 19:30:46 +0800

    249. Level Up Info Data Asset
    -add level up info data asset.

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,30 @@
+// Copyright syxxmyl
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		if (LevelUpInformation.Num() - 1 <= Level)
+		{
+			return Level;
+		}
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			Level += 1;
+		}
+		else
+		{
+			bSearching = false;
+		}
+
+	}
+
+	return Level;
+}
