ea9dcce7afe79117012cfea5cdfffaa0b427fa01
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-04-11 11:11:14 +0800

    365. FireBlast Ability
    -create fire blast ability.

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBlast.h
@@ -0,0 +1,25 @@
+// Copyright syxxmyl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraFireBlast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraFireBlast : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "FireBlast")
+	int32 NumFireBalls = 12;
+};
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -87,6 +87,7 @@ public:
 
 	FGameplayTag Abilities_None;
 	FGameplayTag Abilities_Fire_FireBolt;
+	FGameplayTag Abilities_Fire_FireBlast;
 	FGameplayTag Abilities_Lightning_Electrocute;
 	FGameplayTag Abilities_Arcane_ArcaneShards;
 
