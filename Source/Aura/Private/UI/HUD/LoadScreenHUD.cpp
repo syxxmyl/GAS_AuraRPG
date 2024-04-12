3216d8eae759ed4ccc6d95358dbdbc1d2dfa9b2e
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-04-12 12:47:40 +0800

    384. Constructing a View Model
    -create load screen view model.
    -createload screen widget and add to viewport by cpp.

--- Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -2,4 +2,17 @@
 
 
 #include "UI/HUD/LoadScreenHUD.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+#include "UI/Widget/LoadScreenWidget.h"
 
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+
+	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+}
