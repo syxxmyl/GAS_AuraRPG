29acd0dad5ec0d5e597bb59d3e2b967adc499ee6
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-04-13 12:42:54 +0800

    400. Checkpoints
    -create checkpoint class based on playerstart class.

---
+++ Source/Aura/Private/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,46 @@
+// Copyright syxxmyl
+
+
+#include "Checkpoint/Checkpoint.h"
+#include "Components/SphereComponent.h"
+
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(CheckpointMesh);
+
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void ACheckpoint::HandleGlowEffects()
+{
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+	CheckpointReached(DynamicMaterialInstance);
+}
