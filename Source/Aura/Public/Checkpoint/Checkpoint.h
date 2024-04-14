92c4d1f1c56714ef664494cbfe390ee37687e2d5
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-04-14 12:37:18 +0800

    415. Highlighting Non-Enemies
    -highlight checkpoint actor.

--- Source/Aura/Public/Checkpoint/Checkpoint.h
+++ Source/Aura/Public/Checkpoint/Checkpoint.h
@@ -45,11 +45,10 @@ protected:
 
 	void HandleGlowEffects();
 
-private:
-
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
 
+private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
 };
