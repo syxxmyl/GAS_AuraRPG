0dc31476f376224a962795ddd7e79cdafb2fbf8e
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-04-14 11:27:12 +0800

    412. Different Highlight Colors
    -add different highlight colors to dungeon.

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -6,6 +6,8 @@
 
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
 
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_Target ECollisionChannel::ECC_GameTraceChannel2
